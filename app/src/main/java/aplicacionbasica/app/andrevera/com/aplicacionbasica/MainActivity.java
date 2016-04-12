b3b7d0ebb57fecfda6bcec7c4774ac128ffd63dc
Andrev1611 <stf.1611@gmail.com>
2016-04-11 23:26:56 -0500

    comentario sobre el error al momento de compartir a github

--- app/src/main/java/aplicacionbasica/app/andrevera/com/aplicacionbasica/MainActivity.java
+++ app/src/main/java/aplicacionbasica/app/andrevera/com/aplicacionbasica/MainActivity.java
@@ -9,5 +9,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        * Al querer compartir el proyecto a github VCS-import into version -share github
+        * y sale error de usuario o algo asi se tiene que ejecutar los comandos que indica en el error
+        * y aplicarlos o ejecutarlos en la terminal en la parte inferior de android studio
+        *
+        * */
     }
 }
