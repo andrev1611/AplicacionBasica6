9154963e6c50a77855b169289507f94d95e9a7f3
Andrev1611 <stf.1611@gmail.com>
2016-04-12 16:05:12 -0500

    cambios finales .....

--- app/src/main/java/aplicacionbasica/app/andrevera/com/aplicacionbasica/MainActivity.java
+++ app/src/main/java/aplicacionbasica/app/andrevera/com/aplicacionbasica/MainActivity.java
@@ -1,9 +1,29 @@
 package aplicacionbasica.app.andrevera.com.aplicacionbasica;
 
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvnombre;
+    EditText etnombre;
+    Button btnpresionar;
+    //opciones
+    TextView tvopciones;
+    Button btnopciones;
+    RadioButton rbpaypal,rbvisa;
+    CheckBox cbmastercard,cbbcp;
+    //spinner
+    Spinner spinopciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +35,60 @@ public class MainActivity extends AppCompatActivity {
         * y aplicarlos o ejecutarlos en la terminal en la parte inferior de android studio
         *
         * */
+        tvnombre=(TextView)findViewById(R.id.tvnombre);
+        etnombre=(EditText)findViewById(R.id.etnombre);
+        btnpresionar=(Button)findViewById(R.id.btnpresionar);
+
+        btnpresionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombre=etnombre.getText().toString();
+                tvnombre.setText("Hola "+nombre);
+            }
+        });
+
+        tvopciones=(TextView)findViewById(R.id.tvopcion);
+        btnopciones=(Button)findViewById(R.id.btnopciones);
+        rbpaypal=(RadioButton)findViewById(R.id.rbpaypal);
+        rbvisa=(RadioButton)findViewById(R.id.rbvisa);
+        cbmastercard=(CheckBox)findViewById(R.id.cbmastercard);
+        cbbcp=(CheckBox)findViewById(R.id.cbbcp);
+/*
+* Aquí habrá un temita ya que los radiobuttons se quedarán seleccionados y no se podrá ver
+* el funcionamiento para los chekcbox a noser que se vuelva a ejecutar la aplicación
+* y se empiece probando por los checkbox
+* */
+        btnopciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (rbpaypal.isChecked()){
+                    tvopciones.setText(rbpaypal.getText().toString());
+                }else{
+                    if(rbvisa.isChecked()){
+                        tvopciones.setText(rbvisa.getText());
+                    }else{
+                        if (cbbcp.isChecked()){
+                            //rbvisa.setChecked(false);
+                            //rbpaypal.setChecked(false);
+                            tvopciones.setText(cbbcp.getText());
+                        }else{
+                            if (cbmastercard.isChecked()){
+                                //rbvisa.setChecked(false);
+                                //rbpaypal.setChecked(false);
+                                tvopciones.setText(cbmastercard.getText());
+                            }else {
+                                tvopciones.setText("No seha seleccionado nada");
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        /*
+        * Funcionamiento de spinner
+        * */
+        spinopciones=(Spinner)findViewById(R.id.spinopciones);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this,R.array.opcionesspiner,android.R.layout.simple_spinner_item);
+        spinopciones.setAdapter(adapter);
     }
 }
